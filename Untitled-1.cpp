902ee91e97a892d78f5426bc8e191e42e3607772
ADMIN <dennisgathitu2@gmail.com>
2026-01-30 15:42:32 +0300

    first commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+   body {
+            background-color: lightblue;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: navy;
+        }
+        p {
+            font-size: 16px;
+        }
